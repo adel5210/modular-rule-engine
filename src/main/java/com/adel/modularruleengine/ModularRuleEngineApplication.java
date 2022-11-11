04347cf5766c013f386b36e32295234bb86290b7
adel <adel.albediwy@gmail.com>
2022-11-11 09:35:09 +0300

    Initial commit

---
+++ src/main/java/com/adel/modularruleengine/ModularRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.adel.modularruleengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularRuleEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModularRuleEngineApplication.class, args);
+    }
+
+}
