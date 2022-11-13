e9f740b7d92fb60c69ce510174a3d881d7708601
adel <adel.albediwy@gmail.com>
2022-11-13 07:05:27 +0300

    UI setup

--- modular-rule-engine-frontend/src/main.js
+++ modular-rule-engine-frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import VeeValidate from "vee-validate";
 import {
   BootstrapVue,
@@ -35,6 +34,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
