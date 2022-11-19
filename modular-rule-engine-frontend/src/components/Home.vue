7f0889eb87cc9a2f7fbf02d599fa08566db9ba98
adel <adel.albediwy@gmail.com>
2022-11-19 18:45:22 +0300

    UI setup

--- modular-rule-engine-frontend/src/components/Home.vue
+++ modular-rule-engine-frontend/src/components/Home.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="container" style="height: 100%">
-  <div class="overflow-auto">
-    <b-table striped hover :items="rules"></b-table>
-    <b-pagination-nav :link-gen="linkGen" :number-of-pages="ruleGroupTotalPage" use-router></b-pagination-nav>
-  </div>
+    <div class="overflow-auto" style="padding-top: 20px;">
+      <h2>Modular Rule engine</h2>
+      <b-button v-b-modal.modal-add-rulegroup>Add new rule group</b-button>
+      <b-modal id="modal-add-rulegroup"
+               title="New Rule Group">
+
+      </b-modal>
+      <b-table
+          id="rule-group-table"
+          :items="rulesGroup"
+          :per-page="numberOfElementsPerPage"
+          :current-page="currentPage"
+          small
+      ></b-table>
+      <b-pagination
+          v-if="totalElements > 0"
+          v-model="currentPage"
+          :total-rows="totalElements"
+          :per-page="numberOfElementsPerPage"
+          aria-controls="rule-group-table"
+          @click="getRuleGroup(currentPage)"
+          align="center"
+      ></b-pagination>
+    </div>
   </div>
 </template>
 
@@ -11,22 +31,25 @@
 import DataService from "@/service/DataService";
 
 export default {
-  data(){
+  data() {
     return {
       ruleGroupTotalPage: 0,
-      rules:[]
+      rulesGroup: [],
+      numberOfElementsPerPage: 0,
+      totalElements: 0,
+      currentPage: 1
     }
   },
-  methods:{
-    linkGen(pageNum){
-      console.log(pageNum);
-
-      return '';
-    },
-    getRuleGroup(pageNum){
+  methods: {
+    getRuleGroup(pageNum) {
       console.log(pageNum);
       DataService.getRuleGroups().then(response => {
-        console.log(response);
+        let data = response.data;
+        this.ruleGroupTotalPage = data.totalPages;
+        this.rulesGroup = data.content;
+        this.numberOfElementsPerPage = data.numberOfElements;
+        this.currentPage = data.number;
+        this.totalElements = data.totalElements;
       });
     }
   },
